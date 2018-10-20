d61f1e16f0e6e8925f6f3a89305d69d3a22e5e05
robert <rk@flinkebits.de>
2018-10-20 15:14:49 +0200

    Ava GUI hinzu alle FWs angepasst. Compiled

---
+++ AvaloniaApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using AvaloniaApplication1.ViewModels;
+using AvaloniaApplication1.Views;
+
+namespace AvaloniaApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
